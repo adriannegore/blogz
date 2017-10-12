bdc2e4a9e63d5bbe22a8e3091e027a405341fc59
Adrianne Gore <adrigore89@gmail.com>
2017-10-12 08:36:00 -0500

    Still need to create individual blog page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-
+app.secret_key='ijustwannafinishthisprojectandgotosleep'
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,23 +17,44 @@ class Blog(db.Model):
         self.title = title
         self.body = body
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
-### Currently this only shows the blog posts that are created
-    # TODO add a post form to the html page above the blog posts. Current code support pulling that info and handling request
-    # TODO after we have that functionality working, split into /blog and /newpost. make index redirect to /blog
+@app.route('/newpost', methods=['POST', 'GET'])
+def newpost():
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
+        
+        if not title or not body:
+            flash('Incomplete post. Fill in the rest!')
+            
+            return render_template('newpost.html', title=title, body=body)
+       
         new_blog = Blog(title, body)
         db.session.add(new_blog)
         db.session.commit()
+
+        return return render_template('displaypost.html', title=title, body=body)
+
+    return render_template('newpost.html')
+
+#@app.route('/displaypost')
+#def displaypost():
+ #   title = 'Test title'
+  #  body= 'test body and shit'
+   # return render_template('displaypost.html', title=title, body=body)
+
+
+@app.route('/blog')
+def blog():
 
     blogs = Blog.query.all()
     
     return render_template('blog.html',title="Build a Blog", 
         blogs=blogs)
 
+
+@app.route('/')
+def index():
+    return redirect('/blog')
 
 if __name__ == '__main__':
     app.run()
